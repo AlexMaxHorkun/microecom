c044a2d20551850f7acc85c4ee95a3add27f1895
Oleksandr Gorkun <ogorkun@magento.com>
2020-05-20 15:35:12 -0500

    orders

---
+++ deployment/service/Order/deployment/Db/image/resource/schema.sql
@@ -0,0 +1,12 @@
+create table orders (
+    id uuid not null primary key,
+    customer_id uuid not null,
+    status smallint not null
+);
+
+create table ordered_products (
+    order_id uuid not null,
+    product_id uuid not null,
+    primary key (order_id, product_id),
+    foreign key (order_id) references orders(id) on delete cascade on update cascade
+);
---
+++ deployment/service/Order/deployment/Event/recreate.bat
@@ -0,0 +1,1 @@
+remove.bat & cd image & remove.bat & copy /Y "..\..\..\..\..\..\code\service\Order\Event\target\event-0.0.1-SNAPSHOT.jar" "resource/event-0.0.1-SNAPSHOT.jar" && create.bat && cd .. && create.bat
