53afa5c442616661c4b5d37c1229c8c250f92365
Alexander Gorkun <mindkilleralexs@gmail.com>
2020-09-25 12:55:54 -0500

    grpc updates

--- code/service/Auth/GrpcServer/src/main/java/com/microecom/authservice/grpc/server/model/UserManagerImpl.java
+++ code/service/Auth/GrpcServer/src/main/java/com/microecom/authservice/grpc/server/model/UserManagerImpl.java
@@ -8,6 +8,8 @@ import com.microecom.authservice.model.UserManager;
 import com.microecom.authservice.model.data.UserWithCredentials;
 import com.microecom.authservice.model.exception.InvalidUserDataException;
 import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.validation.ConstraintViolationException;
@@ -20,13 +22,17 @@ public class UserManagerImpl extends UserManagerGrpc.UserManagerImplBase {
 
     private final UserManager userManager;
 
+    private final Logger logger;
+
     public UserManagerImpl(@Autowired UserManager userManager, @Autowired Validator validator) {
         this.userManager = userManager;
         this.validator = validator;
+        this.logger = LoggerFactory.getLogger(getClass());
     }
 
     @Override
     public void createLocal(Users.NewLocalUser request, StreamObserver<Users.User> responseObserver) {
+        logger.info("GRPC.createLocal() requested");
         var newUser = new NewLocalUser(request.getLogin(), request.getPassword(), ZonedDateTime.now());
         if (isValid(newUser, responseObserver)) {
             try {
@@ -39,6 +45,7 @@ public class UserManagerImpl extends UserManagerGrpc.UserManagerImplBase {
                 responseObserver.onNext(result);
                 responseObserver.onCompleted();
             } catch (InvalidUserDataException ex) {
+                logger.warn("GRPC.createLocal() Failed: " + ex.getMessage());
                 responseObserver.onError(ex);
             }
         }
@@ -46,6 +53,7 @@ public class UserManagerImpl extends UserManagerGrpc.UserManagerImplBase {
 
     @Override
     public void find(Users.SearchById request, StreamObserver<Users.User> responseObserver) {
+        logger.info("GRPC.find() requested with " + request.getId());
         var found = userManager.findById(request.getId());
         if (found.isPresent()) {
             var userBuilder = Users.User.newBuilder()
@@ -61,12 +69,14 @@ public class UserManagerImpl extends UserManagerGrpc.UserManagerImplBase {
             responseObserver.onNext(userBuilder.build());
             responseObserver.onCompleted();
         } else {
+            logger.info("GRPC.find(" + request.getId() + ") Not Found!");
             responseObserver.onError(new IllegalArgumentException("User with given ID was not found"));
         }
     }
 
     @Override
     public void updateLocal(Users.LocalUserUpdate request, StreamObserver<Users.User> responseObserver) {
+        logger.info("GRPC.updateLocal() requested for " + request.getId());
         var update = new LocalUserUpdate(request.getId(), request.getPassword());
         if (isValid(update, responseObserver)) {
             try {
@@ -84,6 +94,7 @@ public class UserManagerImpl extends UserManagerGrpc.UserManagerImplBase {
                 );
                 responseObserver.onCompleted();
             } catch (InvalidUserDataException ex) {
+                logger.warn("GRPC.updateLocal(" + request.getId() + ") Update Failed: " + ex.getMessage());
                 responseObserver.onError(ex);
             }
         }
@@ -91,6 +102,7 @@ public class UserManagerImpl extends UserManagerGrpc.UserManagerImplBase {
 
     @Override
     public void delete(Users.SearchById request, StreamObserver<Users.DeletedResult> responseObserver) {
+        logger.info("GRPC.delete() requested for " + request.getId());
         userManager.delete(request.getId());
         responseObserver.onNext(Users.DeletedResult.newBuilder().build());
         responseObserver.onCompleted();
@@ -99,7 +111,9 @@ public class UserManagerImpl extends UserManagerGrpc.UserManagerImplBase {
     private boolean isValid(Object input, StreamObserver response) {
         var violations = validator.validate(input);
         if (!violations.isEmpty()) {
-            response.onError(new ConstraintViolationException(violations));
+            var exception = new ConstraintViolationException(violations);
+            logger.warn("GRPC.isValid() Failed: " + exception.getMessage());
+            response.onError(exception);
 
             return false;
         }
