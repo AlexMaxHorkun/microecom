a030ac7432838020c8b8e93e8339c51096c794c0
Alexander Gorkun <mindkilleralexs@gmail.com>
2020-10-06 11:41:15 -0500

    configured load balancing for auth and customer

--- deployment/service/Auth/deployment/Http/microk8s_shutdown.sh
+++ deployment/service/Auth/deployment/Http/microk8s_shutdown.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/bin/bash -i delete_service.sh && /bin/bash -i microk8s_delete_image.sh && /bin/bash -i delete_image.sh
+/bin/bash -i delete_service.sh
+/bin/bash -i microk8s_delete_image.sh
+/bin/bash -i delete_image.sh
